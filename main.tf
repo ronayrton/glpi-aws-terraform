f137962129c91cb3c5c45a3294bf6cd61f4d29bf
Ronayrton Rocha <ronayrton16@gmail.com>
2024-08-13 15:22:45 -0300

    criação do arquivo main.tf

--- main.tf
+++ main.tf
@@ -1,3 +1,25 @@
 provider "aws" {
-  
+  region = var.aws_region
 }
+
+module "vpc" {
+  source = "./modules/vpc"
+}
+
+module "ec2" {
+  source = "./modules/ec2"
+  vpc_id = module.vpc.vpc_id
+  public_subnet_id = module.vpc.public_subnet_id
+}
+
+module "rds" {
+  source = "./modules/rds"
+  vpc_id = module.vpc.vpc_id
+  private_subnet_ids = module.vpc.private_subnet_ids
+}
+
+module "s3" {
+  source = "./modules/s3"
+}
+
+
